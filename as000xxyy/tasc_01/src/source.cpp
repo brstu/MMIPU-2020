13def895cadb058cd2c046b446f1f58e51555391
Hanna Akalatovich <70771100+as005410@users.noreply.github.com>
2020-09-23 10:47:48 +0300

    feat: add cpp file

---
+++ as000xxyy/tasc_01/src/source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int time[] = { 1, 2, 3, 4 };
+	int t = 1;
+	float y_t, f_t, a, b, c, d, Y_0;
+
+	cout << "enter a, b, c, d";
+	cin >> a >> b >> c >> d;
+	
+	return 0;
+}
