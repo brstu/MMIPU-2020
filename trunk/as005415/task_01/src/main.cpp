ab0c7a7590f5f5ca45a4898566cc00ce844776bd
Mark Shestakov <as005424@g.bstu.by>
2020-09-23 12:06:39 +0300

    [add] Добавлена функция линейного изменения

--- trunk/as005415/task_01/src/main.cpp
+++ trunk/as005415/task_01/src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+void linearEquation(double a, double b, double u_t, double y_t);
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Linear equation:" << std::endl;
+    linearEquation(10, 5, 0.1, 8);
+
     return 0;
+}
+
+void linearEquation(double a, double b, double u_t, double y_t) {
+    double y_t1;
+
+    for (int i = 0; i <= 10; ++i) {
+        y_t1 = a * y_t + b * u_t;
+        std::cout << "[" << i << "] " << "y(t) = " << y_t << "\t\t y(t+1) = " << y_t1 << std::endl;
+        y_t = y_t1;
+    }
 }
