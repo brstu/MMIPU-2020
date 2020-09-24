d63d19c8e70d9df4b5bca7aff4c5310fd517cdd2
Dmitriy Breshchuk <as005404@g.bstu.by>
2020-09-24 18:15:59 +0300

    Change folder name

---
+++ trunk/as005401/task_01/src/Source.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+void linearequation(float a,float b,float y0,float ut,int time) {
+	cout << "1 " << " Yo = " << y0 << endl;
+	for (int i = 0; i < time; i++) {
+		y0 = a * y0 + b * ut;
+		cout << i+2 << " Yo = " << y0 << endl;
+	}
+	cout << endl;
+}
+void nonlinearequation(float a, float b, float c,float d,float y0,float ut, float ut1,float time) {
+	float preivousY = y0;
+	float y = 0;
+	cout << "1 " << " Yo = " << y0 << endl;
+	for (int i = 0; i < time; i++) {
+		y = y0;
+		y0 = a * y0 - b * pow(preivousY, 2) + c * ut + d * sin(ut1);
+		preivousY = y;
+		cout << i + 2 << " Yo = " << y0 << endl;
+	}
+	cout << endl;
+}
+
+int main() {
+	float a = 1;
+	float b = 2;
+	float c = 3;
+	float d = 4;
+	float ut = 3;
+	float y0 = 1;
+	int time = 10;
+	float ut1 = 4;
+	linearequation(a, b, y0, ut, time);
+	nonlinearequation(a, b, c, d, y0, ut, ut1, time);
+	return 0;
+
+}
