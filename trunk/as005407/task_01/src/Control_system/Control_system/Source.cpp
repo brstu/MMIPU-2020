012c7e497d798e6c87fc8cd82869d182c58a7424
Kozik Ilya <as005313@g.bstu.by>
2020-09-25 23:01:27 +0300

    [fix] Changed console output

--- trunk/as005407/task_01/src/Control_system/Control_system/Source.cpp
+++ trunk/as005407/task_01/src/Control_system/Control_system/Source.cpp
@@ -21,10 +21,10 @@ void linear(float y_t, float u_t, int a, float b)
 {
 	float y_t1 = 0;
 	cout << "By linear equation: \n>>>>>\t\t\t\t<<<<<" << endl;
-	cout << "y(t)\t\t\t\ty(t+1)" << endl;
+	cout << "No:\ty(t)\t\t\ty(t+1)" << endl;
 	for (int i = 0; i < 10; i++) {
 		y_t1 = a * y_t + b * u_t;
-		cout << "No:" << i + 1 << "\t" << setprecision(4) << y_t << "\t\t\t" << y_t1 << endl;
+		cout << i + 1 << "\t" << setprecision(4) << y_t << "\t\t\t" << y_t1 << endl;
 		y_t = y_t1;
 	}
 }
@@ -33,10 +33,10 @@ void nonLinear(float y_t, float u_t, int a, float b, int c, int d)
 {
 	float y_t0 = 0, y_t1;
 	cout << "By non-linear equation: \n>>>>>\t\t\t\t<<<<<" << endl;
-	cout << "y(t)\t\t\t\ty(t+1)" << endl;
+	cout << "No:\ty(t)\t\t\ty(t+1)" << endl;
 	for (int i = 0; i < 10; i++) {
 		y_t1 = a * y_t - b * pow(y_t0, 2) + c * u_t + d * sin(u_t);
-		cout <<"No:"<<i+1<<"\t"<< setprecision(4) << y_t << "\t\t\t" << y_t1 << endl;
+		cout <<i+1<<"\t"<< setprecision(4) << y_t << "\t\t\t" << y_t1 << endl;
 		y_t0 = y_t;
 		y_t = y_t1;
 	}
