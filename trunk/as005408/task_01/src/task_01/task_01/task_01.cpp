8f315a46948c44a29b85d6cf7f8cdbf0272f5fa1
Alexander Kunitskiy <as005314@g.bstu.by>
2020-10-01 19:09:25 +0300

    [add] Linear model function

--- trunk/as005408/task_01/src/task_01/task_01/task_01.cpp
+++ trunk/as005408/task_01/src/task_01/task_01/task_01.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
+using namespace std;
+
+void linearEquation(double a, double b, double y_t, double u_t);
+void nonLinearEquation(double a, double b, double c, double d, double y_t, double u_t);
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	linearEquation(1,0.0003,16,27);
 }
+
+void linearEquation(double a, double b, double y_t, double u_t) {
+
+	cout << "Linear model: " << endl;
+
+	for (int i = 0; i < 10; i++)
+	{
+		double y_t1 = a*y_t + b*u_t;
+		cout << "[" << i << "]" << " " << "y(t)= " << y_t << " " << "y(t+1)= " << y_t1 << endl;
+		y_t = y_t1;
+	}
+}
+
+/*void nonLinearEquation(double a, double b, double c, double d, double y_t, double u_t) {
+
+}*/
