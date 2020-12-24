5e37bfe8fc59b26e146a802d1b47806577780f19
Alina Grigoruk <70770852+as005402@users.noreply.github.com>
2020-12-24 09:55:18 +0300

    [Task_03] Work with AXC F 2152 controller (#153)

---
+++ trunk/as005402/task_03/src/3lab.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 3lab.cpp
+// Author      : fatona
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
