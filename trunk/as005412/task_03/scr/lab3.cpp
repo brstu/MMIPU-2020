24bcdee9c8fc0a5d5301636c33a57f59ddafcfe8
Alexander Safrin <70979250+as005412@users.noreply.github.com>
2020-12-25 21:23:10 +0300

    [Task 03] Completed work with controller (#171)

---
+++ trunk/as005412/task_03/scr/lab3.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : lab3.cpp
+// Author      : EL Grapes
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello Safrin" << endl; // prints !!!Hello World!!!
+	return 0;
+}
